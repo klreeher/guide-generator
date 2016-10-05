109398e711e46fa352588bf014ada79481647474
Crhistian Ramirez <cramirez@four51.com>
2016-10-05 10:58:55 -0500

    OC-1681 set up preview state to pull in generated guide and display

--- src/app/preview/preview.js
+++ src/app/preview/preview.js
@@ -1,3 +1,20 @@
 angular.module('orderCloud')
     .config(PreviewConfig)
-    .controller('PreviewCtrl')
+    .controller('PreviewCtrl', PreviewController)
+;
+
+function PreviewConfig($stateProvider){
+    $stateProvider
+        .state('preview', {
+            parent:'base',
+            url:'/preview',
+            templateUrl:'preview/templates/preview.tpl.html',
+            controller:'PreviewCtrl',
+            controllerAs:'preview'
+        })
+    ;
+}
+
+function PreviewController(){
+    var vm = this;
+}
